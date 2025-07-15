5facfd360d8e02a00817fbdf8999d1cbc27b1a81
Giulia Pascucci <giuliapascuccii03@gmail.com>
2025-07-15 16:46:57 +0200

    ho effettuato il Program01 e il Program02 dell'esercizio 03

--- 03_Eserc_Stringhe/src/it/akt/Program01.java
+++ 03_Eserc_Stringhe/src/it/akt/Program01.java
@@ -4,10 +4,16 @@ public class Program01 {
 
 	public static void main(String[] args) {
 		
-
+	String esercizio1 = "Copiare il testo di questo esercizio in una Stringa.Contare quante volte compare la lettera e nel testo di questo esercizio e restituire l'informazione a console.";
+	int conteggio=0;
+	int i= esercizio1.indexOf("e");
 		
-		
-		
-		
+	while (i !=-1)	{
+		i=esercizio1.indexOf("e",i+1);
+		conteggio++;
+	}
+	
+	System.out.println("La lettera 'e' compare " +conteggio+ " volte nel testo.");
+	
 	}
 }
--- 03_Eserc_Stringhe/src/it/akt/Program02.java
+++ 03_Eserc_Stringhe/src/it/akt/Program02.java
@@ -1,10 +1,20 @@
 package it.akt;
 
-public class Program02 {
+import java.util.Scanner;
 
+public class Program02 {
+//Ricevete una stringa in ingresso da tastiera (Scanner)
+	//Scrivi un programma che inverte l'ordine dei caratteri in una stringa e la stampa al contrario.
 	public static void main(String[] args) {
 		
-
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("inserire una parola o una frase:");
+		String stringa= scanner.nextLine();
+		
+		StringBuilder str = new StringBuilder(stringa);
+		System.out.println(str.reverse());
+		
+		scanner.close();
 	}
 
 }
