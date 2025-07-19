ea31bb2900cb40ca3a18ebf04524076846289935
Giulia Pascucci <giuliapascuccii03@gmail.com>
2025-07-19 15:32:08 +0200

    ho eseguito il Program01 dell'esercizio 04

--- 04_Eserc_Collezioni/src/it/akt/Program01List.java
+++ 04_Eserc_Collezioni/src/it/akt/Program01List.java
@@ -1,10 +1,41 @@
 package it.akt;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program01List {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		List <String> nomiStudenti = new ArrayList<>();
+		
+		nomiStudenti.add("Alice");
+		nomiStudenti.add("Bob");
+		nomiStudenti.add("Charlie");
+		nomiStudenti.add("Alice");
+		
+		//stampo tutti i nomi
+		for (int i = 0; i < nomiStudenti.size(); i++)								
+			System.out.print(nomiStudenti.get(i) + " ");
+		
+		//elimino prima occorenza di alice
+		nomiStudenti.remove(nomiStudenti.indexOf("Alice"));
+		
+		System.out.println();
+		
+		//ristampo con nuove modifiche
+		for (int i = 0; i < nomiStudenti.size(); i++)								
+			System.out.print(nomiStudenti.get(i) + " ");
+		
+		System.out.println();
+		
+		//verifica presenza di "Bob" nella lista
+		if (nomiStudenti.contains("Bob")) {
+			System.out.println("La lista contiene il nome Bob!");
+		}
+		
+		//stampo dimensione attuale lista
+		System.out.println("La dimensione della lista è di: " + nomiStudenti.size() + " nomi");
 	}
 
 }
