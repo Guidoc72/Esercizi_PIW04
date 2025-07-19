33d365f0c3c254afcd4ced7e8985b2dc8c5b4607
Giulia Pascucci <giuliapascuccii03@gmail.com>
2025-07-19 14:51:44 +0200

    Ho completato tutti e tre i programmi dell'esercizio 04

---
+++ 04_Eserc_Collezioni/src/it/akt/Program01List.java
@@ -0,0 +1,10 @@
+package it.akt;
+
+public class Program01List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ 04_Eserc_Collezioni/src/it/akt/Program02Set.java
@@ -0,0 +1,10 @@
+package it.akt;
+
+public class Program02Set {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
